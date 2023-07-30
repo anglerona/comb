a91350b1de083fdbc5b8566fc77157f4785139ae
Angelina <angelina03wu@gmail.com>
2023-07-30 16:45:21 -0700

    structure and theme

---
+++ constants/theme.js
@@ -0,0 +1,52 @@
+const COLORS = {
+    primary: "#265149",
+    secondary: "#42625E",
+    tertiary: "#FF6086",
+  
+    gray: "#829A99",
+    gray2: "#C0C8C8",
+  
+    white: "#F3F4F8",
+    lightWhite: "#FAFAFC",
+  };
+  
+  const FONT = {
+    regular: "DMRegular",
+    medium: "DMMedium",
+    bold: "DMBold",
+  };
+  
+  const SIZES = {
+    xSmall: 10,
+    small: 12,
+    medium: 16,
+    large: 20,
+    xLarge: 24,
+    xxLarge: 32,
+  };
+  
+  const SHADOWS = {
+    small: {
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 2,
+    },
+    medium: {
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 5.84,
+      elevation: 5,
+    },
+  };
+  
+  export { COLORS, FONT, SIZES, SHADOWS };
+  
